59eb596bfd24024d4fac175c4b6ceb8d7aae7aff
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-05-03 17:29:57 +0530

    add: front page

---
+++ zoomlite/app/(root)/meetings/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingPage = ({ params } : {params: {id: string }}) => {
+  return (
+    <div>
+      Meeting Room : #{params.id}
+    </div>
+  )
+}
+
+export default MeetingPage
