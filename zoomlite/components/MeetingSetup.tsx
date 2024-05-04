60765cae5eb108f2c9ebc88a9d662d22a5ea5ab4
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-05-04 13:30:14 +0530

    feat: meetingsetup and meetingroom

---
+++ zoomlite/components/MeetingSetup.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { DeviceSettings, VideoPreview, useCall } from '@stream-io/video-react-sdk'
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+
+const MeetingSetup = ({setIsSetupComplete}:{setIsSetupComplete: (val: boolean) => void}) => {
+    const [isMicCamToggledon, setIsMicCamToggledOn] = useState(false)
+    const call = useCall();
+    if(!call) {
+        throw new Error("useCall must be used within StreamCall component");
+    }
+    useEffect(() => {
+        if(isMicCamToggledon){
+            call?.camera.disable();
+            call?.microphone.disable();
+        } else {
+            call?.camera.enable();
+            call?.microphone.enable();
+        }
+    }, [isMicCamToggledon, call?.camera, call?.microphone])
+  return (
+    <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
+    <h1 className='text-2xl font-bold'>Setup</h1>
+    <VideoPreview />
+    <div className='flex h-16 items-center justify-center gap-3'>
+        <label className='flex items-center justify-center gap-2 font-medium'>
+            <input type='checkbox' checked={isMicCamToggledon} onChange={(e) => setIsMicCamToggledOn(e.target.checked)}/>
+            Join with mic and camera off
+        </label>
+        <DeviceSettings />
+        <Button className='rounded-md bg-green-500 px-4 py-2.5' onClick={() => {
+            call.join();
+            setIsSetupComplete(true)
+        }}>
+            Join Meeting
+        </Button>
+    </div>
+    </div>
+  )
+}
+
+export default MeetingSetup
